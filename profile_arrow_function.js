d729df27d7cc2c234cdc92cb5508e75571422408
lita anjelita <141709031+litaanjelita@users.noreply.github.com>
2023-10-02 13:58:23 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,7 @@
+const login = (namaDepan,namaBelakang,email) => {
+  console.log("Nama : " + namaDepan+" "+ namaBelakang)
+  console.log("email :"+ email)
+  console.log("Login Screen")
+}
+
+login("Lita","Anjelita","222310265@smkn4-tsm.sch.id")
---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+function home(namaDepan,namaBelakang,email) {
+  console.log("Nama : " + namaDepan +" "+ namaBelakang)
+  console.log("email :"+ email)
+  console.log("Selamat Datang di Home Screen")
+}
+
+home("Lita","Anjelita","222310265@smkn4-tsm.sch.id")
